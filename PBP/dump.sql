81a70eb47e747b66cfd235ebccadb79cba7eebdf
shcke <djapedja@gmail.com>
2016-02-18 10:01:39 +0100

    prepravio putanju na dump.sql

--- PBP/dump.sql
+++ PBP/dump.sql
@@ -1,36 +1,36 @@
 use	gradjevinskaFirma;
 
-LOAD DATA INFILE 'ISGradjevinskeFirme/PBP/podaci/zaposleni.txt' INTO TABLE zaposleni
+LOAD DATA INFILE './podaci/zaposleni.txt' INTO TABLE zaposleni
   FIELDS TERMINATED BY ';';
-LOAD DATA INFILE 'ISGradjevinskeFirme/PBP/podaci/ovlascenja.txt' INTO TABLE ovlascenja
+LOAD DATA INFILE './podaci/ovlascenja.txt' INTO TABLE ovlascenja
   FIELDS TERMINATED BY ';';
-LOAD DATA INFILE 'ISGradjevinskeFirme/PBP/podaci/pozicije.txt' INTO TABLE pozicije
+LOAD DATA INFILE './podaci/pozicije.txt' INTO TABLE pozicije
   FIELDS TERMINATED BY ';';
-LOAD DATA INFILE 'ISGradjevinskeFirme/PBP/podaci/projekti.txt' INTO TABLE projekti
+LOAD DATA INFILE './PBP/podaci/projekti.txt' INTO TABLE projekti
   FIELDS TERMINATED BY ';';
-LOAD DATA INFILE 'ISGradjevinskeFirme/PBP/podaci/gradjevinskiObjekti.txt' INTO TABLE gradjevinskiObjekti
+LOAD DATA INFILE './podaci/gradjevinskiObjekti.txt' INTO TABLE gradjevinskiObjekti
   FIELDS TERMINATED BY ';';
-LOAD DATA INFILE 'ISGradjevinskeFirme/PBP/podaci/dobavljaci.txt' INTO TABLE dobavljaci
+LOAD DATA INFILE './podaci/dobavljaci.txt' INTO TABLE dobavljaci
   FIELDS TERMINATED BY ';';
-LOAD DATA INFILE 'ISGradjevinskeFirme/PBP/podaci/materijali.txt' INTO TABLE materijali
+LOAD DATA INFILE './podaci/materijali.txt' INTO TABLE materijali
   FIELDS TERMINATED BY ';';
-LOAD DATA INFILE 'ISGradjevinskeFirme/PBP/podaci/skladiste.txt' INTO TABLE skladiste
+LOAD DATA INFILE './podaci/skladiste.txt' INTO TABLE skladiste
   FIELDS TERMINATED BY ';';
-LOAD DATA INFILE 'ISGradjevinskeFirme/PBP/podaci/nabavkeMaterijala.txt' INTO TABLE nabavkeMaterijala
+LOAD DATA INFILE './podaci/nabavkeMaterijala.txt' INTO TABLE nabavkeMaterijala
   FIELDS TERMINATED BY ';';
-LOAD DATA INFILE 'ISGradjevinskeFirme/PBP/podaci/stavkeNabavkeMaterijala.txt' INTO TABLE stavkeNabavkeMaterijala
+LOAD DATA INFILE './podaci/stavkeNabavkeMaterijala.txt' INTO TABLE stavkeNabavkeMaterijala
   FIELDS TERMINATED BY ';';
-LOAD DATA INFILE 'ISGradjevinskeFirme/PBP/podaci/prodaja.txt' INTO TABLE prodaja
+LOAD DATA INFILE './podaci/prodaja.txt' INTO TABLE prodaja
   FIELDS TERMINATED BY ';';
-LOAD DATA INFILE 'ISGradjevinskeFirme/PBP/podaci/masine.txt' INTO TABLE masine
+LOAD DATA INFILE './podaci/masine.txt' INTO TABLE masine
   FIELDS TERMINATED BY ';';
-LOAD DATA INFILE 'ISGradjevinskeFirme/PBP/podaci/nabavkeMasina.txt' INTO TABLE nabavkeMasina
+LOAD DATA INFILE './podaci/nabavkeMasina.txt' INTO TABLE nabavkeMasina
   FIELDS TERMINATED BY ';';
-LOAD DATA INFILE 'ISGradjevinskeFirme/PBP/podaci/stavkeNabavkeMasina.txt' INTO TABLE stavkeNabavkeMasina
+LOAD DATA INFILE './podaci/stavkeNabavkeMasina.txt' INTO TABLE stavkeNabavkeMasina
   FIELDS TERMINATED BY ';';
-LOAD DATA INFILE 'ISGradjevinskeFirme/PBP/podaci/radovi.txt' INTO TABLE radovi
+LOAD DATA INFILE './podaci/radovi.txt' INTO TABLE radovi
   FIELDS TERMINATED BY ';';
-LOAD DATA INFILE 'ISGradjevinskeFirme/PBP/podaci/podizvodjaci.txt' INTO TABLE podizvodjaci
+LOAD DATA INFILE './podaci/podizvodjaci.txt' INTO TABLE podizvodjaci
   FIELDS TERMINATED BY ';';
-LOAD DATA INFILE 'ISGradjevinskeFirme/PBP/podaci/ugovoriSaPodizvodjacima.txt' INTO TABLE ugovoriSaPodizvodjacima
+LOAD DATA INFILE './podaci/ugovoriSaPodizvodjacima.txt' INTO TABLE ugovoriSaPodizvodjacima
   FIELDS TERMINATED BY ';';
